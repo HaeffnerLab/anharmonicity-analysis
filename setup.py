5d3d5f0e4e3edfc24132f1088e6ad6cf33c260a1
andris-huang <andrewhz@berkeley.edu>
2025-06-20 19:57:43 -0700

    init commit

---
+++ setup.py
@@ -0,0 +1,47 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+from setuptools import find_packages
+from setuptools import setup
+
+description="Analyzing anharmonicity of traps for charged particles"
+
+setup(
+    name="esim",
+    version="1.0.0",
+    description="Library for analyzing charged-particle trap anharmonicity",
+    long_description=description,
+    author="Electron",
+    keywords=["Quantum Information Processing", "Numerical Simulation"],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "future",
+        "numpy",
+        "scipy",
+        "pandas",
+        "setuptools",
+        "matplotlib",
+        'tqdm',
+        # 'optuna',
+        'plotly',
+        'scikit-learn',
+        #'sphericart',
+        'pyvista',
+        'xarray',
+        'cvxpy',
+        'sympy',
+        'juliacall'
+    ],
+    package_data = {
+        "esim": ["utils/*.py"]
+    },
+    setup_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+    ],
+    scripts=[
+    ],
+)
